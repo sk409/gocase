08805a003e8161f6eb37286ee6b3140fce58f8fb
Shoutokobayashi <deepstrap353@gmail.com>
2020-01-19 14:30:33 +0900

    dev

--- case.go
+++ case.go
@@ -1,15 +1,22 @@
 package gocase
 
-func UpperCamelCase(b []byte, golike bool) []byte {
-	b = firstTransformer.upperCamelCase(b)
-	b = snakeTransformer.upperCamelCase(b)
+func lowerCamelCase(b []byte, golike bool) []byte {
+	b = upperCamelTransformer.lowerCamelCase(b)
 	if golike {
-		b = golikeTransformer.upperCamelCase(b)
+		b = golikeTransformer.transform(b)
 	}
 	return b
 }
 
 func SnakeCase(b []byte) []byte {
 	b = upperCamelTransformer.snakeCase(b)
+	return b
+}
+
+func UpperCamelCase(b []byte, golike bool) []byte {
+	b = snakeTransformer.upperCamelCase(b)
+	if golike {
+		b = golikeTransformer.transform(b)
+	}
 	return b
 }
--- transformers.go
+++ transformers.go
@@ -5,22 +5,10 @@ import (
 	"strings"
 )
 
-type first struct {
-}
-
-func (f *first) upperCamelCase(b []byte) []byte {
-	if len(b) == 0 {
-		return []byte{}
-	}
-	runes := []rune(string(b))
-	runes[0] = rune(strings.ToUpper(string(b[0]))[0])
-	return []byte(string(runes))
-}
-
 type golike struct {
 }
 
-func (g *golike) upperCamelCase(b []byte) []byte {
+func (g *golike) transform(b []byte) []byte {
 	str := string(b)
 	str = strings.ReplaceAll(str, "Id", "ID")
 	str = strings.ReplaceAll(str, "Url", "URL")
@@ -31,7 +19,11 @@ type snake struct {
 }
 
 func (s *snake) upperCamelCase(b []byte) []byte {
+	if len(b) == 0 {
+		return []byte{}
+	}
 	runes := []rune(string(b))
+	runes[0] = rune(strings.ToUpper(string(b[0]))[0])
 	regex := regexp.MustCompile("_([a-z])")
 	matches := regex.FindAllSubmatchIndex(b, -1)
 	for _, match := range matches {
@@ -45,6 +37,15 @@ func (s *snake) upperCamelCase(b []byte) []byte {
 type upperCamel struct {
 }
 
+func (u *upperCamel) lowerCamelCase(b []byte) []byte {
+	if len(b) == 0 {
+		return []byte{}
+	}
+	runes := []rune(string(b))
+	runes[0] = rune(strings.ToLower(string(b[0]))[0])
+	return []byte(string(runes))
+}
+
 func (u *upperCamel) snakeCase(b []byte) []byte {
 	regex := regexp.MustCompile("(.)([A-Z])")
 	snake := regex.ReplaceAll(b, []byte("${1}_${2}"))
@@ -52,7 +53,6 @@ func (u *upperCamel) snakeCase(b []byte) []byte {
 }
 
 var (
-	firstTransformer      = first{}
 	golikeTransformer     = golike{}
 	snakeTransformer      = snake{}
 	upperCamelTransformer = upperCamel{}
